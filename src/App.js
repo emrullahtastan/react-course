328b5034ebb4d71f3ece0d5708fae2a06dee6678
emrullahtastan <emrullah69@gmail.com>
2022-09-30 15:43:41 +0300

    add prettier and husky

--- src/App.js
+++ src/App.js
@@ -1,70 +1,78 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
+  let listTabs = [
+    { key: "a", title: "A" },
+    { key: "b", title: "B" },
+  ];
 
-    let listTabs = [{key: "a", title: "A"}, {key: "b", title: "B"}];
+  let list = [
+    {
+      id: 1,
+      title: "X",
+      img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg",
+    },
+    {
+      id: 2,
+      title: "Z",
+      img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg",
+    },
+    {
+      id: 2,
+      title: "Z",
+      img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg",
+    },
+    {
+      id: 2,
+      title: "Z",
+      img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg",
+    },
+    {
+      id: 2,
+      title: "Z",
+      img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg",
+    },
+  ];
 
-    let list = [{
-        id: 1,
-        title: "X",
-        img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg"
-    }, {
-        id: 2,
-        title: "Z",
-        img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg"
-    }, {
-        id: 2,
-        title: "Z",
-        img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg"
-    }, {
-        id: 2,
-        title: "Z",
-        img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg"
-    }, {
-        id: 2,
-        title: "Z",
-        img: "https://cdn-i.pr.trt.com.tr/trtsales/g/images/content/1-wide.jpeg"
-    }];
-
-    return (
-        <div className="App">
-            <div className={"container"}>
-                <div className={"tabs"}>
-                    <div className={"tabs__header"}>
-                        {listTabs.map(function (o, i) {
-                            return (
-                                <div className={"tabs__header__item"} key={i}>{o.title}</div>
-                            );
-                        }, this)}
-                    </div>
-                    <div className={"tabs__content"}>
-                        <div className={"tabs__content__item"}>
-                            <List key={"list"} list={list}/>
-                        </div>
-                    </div>
+  return (
+    <div className="App">
+      <div className={"container"}>
+        <div className={"tabs"}>
+          <div className={"tabs__header"}>
+            {listTabs.map(function (o, i) {
+              return (
+                <div className={"tabs__header__item"} key={i}>
+                  {o.title}
                 </div>
+              );
+            }, this)}
+          </div>
+          <div className={"tabs__content"}>
+            <div className={"tabs__content__item"}>
+              <List key={"list"} list={list} />
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 function List(props) {
-    return (
-        <div className={"list-container"}>
-            {props.list.map(function (o, i) {
-                return (
-                    <div className={"list-container__item"} key={i}>
-                        <div className={"list-container__item__img"}>
-                            <img src={o.img} alt={""}/>
-                        </div>
-                        <h1 className={"list-container__item__title"}>
-                            {o.title}
-                        </h1>
-                    </div>
-                );
-            }, this)}
-        </div>
-    );
+  return (
+    <div className={"list-container"}>
+      {props.list.map(function (o, i) {
+        return (
+          <div className={"list-container__item"} key={i}>
+            <div className={"list-container__item__img"}>
+              <img src={o.img} alt={""} />
+            </div>
+            <h1 className={"list-container__item__title"}>{o.title}</h1>
+          </div>
+        );
+      }, this)}
+    </div>
+  );
 }
 
 export default App;
